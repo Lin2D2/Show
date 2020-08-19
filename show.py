ea84ed13af74f5f84922b3ab8975335ad357e7aa
space <linus@thebehrens.net>
2020-08-20 00:14:33 +0200

    fixed all mayor errors

--- show.py
+++ show.py
@@ -1,19 +1,21 @@
 import pygame
 import os
+import logging
 from PIL import Image
 
 
-class pyscope:
+class PyScope:
     screen = None
 
-    def __init__(self):
+    def __init__(self, logger):
+        self.logger = logger
         "Ininitializes a new pygame screen using the framebuffer"
         # Based on "Python GUI in Linux frame buffer"
         # http://www.karoltomala.com/blog/?p=679
         disp_no = os.getenv("DISPLAY")
         if disp_no:
-            print(
-            "I'm running under X display = {0}".format(disp_no))
+            self.logger.info(
+                "I'm running under X display = {0}".format(disp_no))
 
         # Check which frame buffer drivers are available
         # Start with fbcon since directfb hangs with composite output
@@ -26,8 +28,8 @@ class pyscope:
             try:
                 pygame.display.init()
             except pygame.error:
-                print(
-                'Driver: {0} failed.'.format(driver))
+                self.logger.warning(
+                    'Driver: {0} failed.'.format(driver))
                 continue
             found = True
             break
@@ -36,8 +38,8 @@ class pyscope:
             raise Exception('No suitable video driver found!')
 
         size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        print(
-        "Framebuffer size: %d x %d" % (size[0], size[1]))
+        self.logger.info(
+            "Framebuffer size: %d x %d" % (size[0], size[1]))
         self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
         # Clear the screen to start
         self.screen.fill((0, 0, 0))
@@ -47,14 +49,8 @@ class pyscope:
         pygame.display.update()
 
     def __del__(self):
-        "Destructor to make sure pygame shuts down, etc."
-
-    def test(self):
-        # Fill the screen with red (255, 0, 0)
-        red = (255, 0, 0)
-        self.screen.fill(red)
-        # Update the display
-        pygame.display.update()
+        # Destructor to make sure pygame shuts down, etc.
+        pass
 
 
 class Button:
@@ -77,19 +73,32 @@ class Button:
             font = pygame.font.SysFont("Corbel", 40)
             text = font.render(self.text, 1, (0, 0, 0))
             win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+                self.x + (int(self.width / 2 - text.get_width() / 2)),
+                self.y + (int(self.height / 2 - text.get_height() / 2))))
 
-    def isOver(self, pos):
+    def is_over(self, pos):
         # Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
                 return True
 
         return False
 
 
 class ImageShow:
-    def __init__(self, screen):
+    def __init__(self):
+        self.logger = logging.getLogger("Discord_Bot_Logger")
+        self.logger.setLevel(logging.INFO)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+        formatter = logging.Formatter("%(asctime)s: :%(levelname)s: %(message)s",
+                                      "%Y-%m-%d %H:%M:%S")
+
+        ch.setFormatter(formatter)
+
+        self.logger.addHandler(ch)
         # init
         pygame.init()
         # Font
@@ -101,12 +110,18 @@ class ImageShow:
         self.color_black = pygame.Color("black")
         self.color_grey = pygame.Color("grey")
         self.color_orange = pygame.Color("orange")
+        self.color_yellow = pygame.Color("yellow")
 
-        # screen = pygame.display.set_mode([1920, 1080], pygame.FULLSCREEN)
-        # self.screen = pygame.display.set_mode([1920, 1080], pygame.FULLSCREEN)
-        self.screen = screen.screen
+        try:
+            # Create an instance of the PyScope class
+            scope = PyScope(self.logger)
+            self.screen = scope.screen
+        except Exception as error:
+            self.logger.warning(f'error trying to run in frame buffer: {error}')
+            self.screen = pygame.display.set_mode([1920, 1080], pygame.FULLSCREEN)
         # Clock
         self.screen_clock = pygame.time.Clock()
+        self.tick_rate = 10
         # set witdh and height
         self.SW = self.screen.get_width()
         self.SH = self.screen.get_height()
@@ -137,17 +152,20 @@ class ImageShow:
                                             self.quit_button_dimension[0] / 2 + self.quit_button_dimension[0] +
                                             self.margin), int(self.SH - self.forward_button_dimension[1] - self.margin))
         self.forward_button = Button(self.color_white, self.forward_button_location[0], self.forward_button_location[1],
-                                  self.forward_button_dimension[0], self.forward_button_dimension[1], ">>")
+                                     self.forward_button_dimension[0], self.forward_button_dimension[1], ">>")
 
         self.rotate_button_dimension = (50, 30)
         self.rotate_button_location = (int(self.forward_button_location[0] + self.forward_button_dimension[0] +
                                            self.margin), int(self.SH - self.rotate_button_dimension[1] - self.margin))
         self.rotate_button = Button(self.color_white, self.rotate_button_location[0], self.rotate_button_location[1],
-                                  self.rotate_button_dimension[0], self.rotate_button_dimension[1], "90°")
+                                    self.rotate_button_dimension[0], self.rotate_button_dimension[1], "90°")
 
         # run
         self.draw_update(self.images[self.current_image_i])
-        self.Main()
+        try:
+            self.main()
+        except pygame.error as error:
+            self.logger.warning(error)
 
     def draw_update(self, image, image_rotate=0):
         # fill screen
@@ -161,18 +179,23 @@ class ImageShow:
         aspect_ratio = size[0] / size[1]
 
         if size[0] > size[1]:
-            # TODO changes aspect ratio
+            self.logger.debug("pictures width is lager then height")
             with_fac = self.SW / size[0]
             height_res = with_fac * size[1]
             picture_width = int(self.SW)
             picture_height = int(height_res)
             if picture_height > (self.SH - (self.margin * 3)):
-                height_fac = picture_height / size[1]
+                height_fac = (self.SH - (self.margin * 3)) / size[1]
                 width_res = height_fac * size[0]
+                height_res = height_fac * size[1]
                 picture_width = int(width_res)
-                picture_height = int(self.SH - (self.margin * 3))
+                picture_height = int(height_res)
+            new_aspect_ratio = picture_width / picture_height
+            if round(new_aspect_ratio, 2) != round(aspect_ratio, 2):
+                self.logger.warning(f"aspect_ratio changed!!!\nold: {aspect_ratio}\nnew: {new_aspect_ratio}")
 
         else:
+            self.logger.debug("pictures height is lager then width")
             height_fac = (self.SH - (self.margin * 3)) / size[1]
             width_res = height_fac * size[0]
             picture_width = int(width_res)
@@ -186,7 +209,7 @@ class ImageShow:
         picture = pygame.image.fromstring(data, size, mode)
         # draw image
         image_rect = picture.get_rect()
-        image_rect.center = ((int(self.SW / 2), int(self.SH / 2 - self.margin * 4)))
+        image_rect.center = (int(self.SW / 2), int(self.SH / 2 - self.margin * 4))
         self.screen.blit(picture, image_rect)
         # draw button
         self.back_button.draw(self.screen)
@@ -196,7 +219,7 @@ class ImageShow:
         # update
         pygame.display.update()
 
-    def Main(self):
+    def main(self):
         running = True
 
         while running:
@@ -211,9 +234,11 @@ class ImageShow:
                         pygame.quit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     # quit button
-                    if (self.quit_button_location[0] <= mouse[0] <= self.quit_button_location[0] + self.quit_button_dimension[0]
+                    if (self.quit_button_location[0] <= mouse[0] <= self.quit_button_location[0] +
+                            self.quit_button_dimension[0]
                             and
-                            self.quit_button_location[1] <= mouse[1] <= self.quit_button_location[1] + self.quit_button_dimension[1]):
+                            self.quit_button_location[1] <= mouse[1] <= self.quit_button_location[1] +
+                            self.quit_button_dimension[1]):
                         pygame.quit()
                     # forward button
                     elif (self.forward_button_location[0] <= mouse[0] <= self.forward_button_location[0] +
@@ -226,28 +251,33 @@ class ImageShow:
                             self.current_image_i += 1
                         else:
                             self.draw_update(self.images[0])
-                            current_image_i = 0
+                            self.current_image_i = 0
                     # back button
-                    elif (self.back_button_location[0] <= mouse[0] <= self.back_button_location[0] + self.back_button_dimension[0]
+                    elif (self.back_button_location[0] <= mouse[0] <= self.back_button_location[0] +
+                          self.back_button_dimension[0]
                           and
-                          self.back_button_location[1] <= mouse[1] <= self.back_button_location[1] + self.back_button_dimension[1]):
+                          self.back_button_location[1] <= mouse[1] <= self.back_button_location[1] +
+                          self.back_button_dimension[1]):
                         if self.current_image_i - 1 >= 0:
                             self.draw_update(self.images[self.current_image_i - 1])
                             self.current_image_i -= 1
                         else:
                             self.draw_update(self.images[-1])
-                            current_image_i = len(self.images) - 1
+                            self.current_image_i = len(self.images) - 1
                     # rotate button
-                    elif (self.rotate_button_location[0] <= mouse[0] <= self.rotate_button_location[0] + self.rotate_button_dimension[
-                        0]
+                    elif (self.rotate_button_location[0] <= mouse[0] <= self.rotate_button_location[0] +
+                          self.rotate_button_dimension[
+                              0]
                           and
-                          self.rotate_button_location[1] <= mouse[1] <= self.rotate_button_location[1] + self.rotate_button_dimension[
+                          self.rotate_button_location[1] <= mouse[1] <= self.rotate_button_location[1] +
+                          self.rotate_button_dimension[
                               1]):
                         self.draw_update(self.images[self.current_image_i], image_rotate=90)
-            self.screen_clock.tick(5)
+            self.screen_clock.tick(self.tick_rate)
+            fps_counter = Button(self.color_yellow, self.SW - 60, 0, 60, 40,
+                                 text=str(round(self.screen_clock.get_fps(), 2)))
+            fps_counter.draw(self.screen)
+            pygame.display.update()
 
 
-# Create an instance of the PyScope class
-scope = pyscope()
-
-image_show = ImageShow(scope)
+image_show = ImageShow()
